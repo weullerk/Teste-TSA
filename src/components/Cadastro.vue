c6814ed5dc428c5b954faec9a84f78073fcc4f1b
Weuller Krysthian <wrodrigues@aiko.digital>
2020-08-04 18:39:31 -0300

    Iniciando repositório com arquivos do teste

---
+++ src/components/Cadastro.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Cadastro</h1>
+</template>
+
+<script>
+export default {
+  name: 'Cadastro'
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Lista.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Lista</h1>
+</template>
+
+<script>
+export default {
+  name: 'Lista'
+}
+</script>
+
+<style scoped>
+
+</style>
